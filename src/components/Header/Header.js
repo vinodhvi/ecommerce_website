9c7327f190b16737cf50ed42cf9af028666dfa4a
vinodhvi <vinodh1597@gmail.com>
2024-04-19 21:24:30 +0530

    initial

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Header.css";
 import SearchIcon from '@mui/icons-material/Search';
 import Select from "../Selectdrop/Select";
+import axios from "axios";
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import compare from './../images/compare.svg'
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import { Button } from "@mui/material";
 const Header = () => {
+  const [Categories , setCategories] = useState([
+    
+    'Milks and Dhals',
+    'Vegetables',
+    'Ice Cream',
+    'Noodeels & Rice',
+    'Noodeels & Rice',
+    'Noodeels & Rice',
+    'Noodeels & Rice',
+    'Noodeels & Rice',
+  ]);
+
+  const countryList=[];
+
+  const getCountry = async()=> {
+    try{
+      await axios.get('https://countriesnow.space/api/v0.1/countries/').then((res)=>{
+        if(res!==null){
+          // console.log(res.data.data);
+          res.data.data.map((item, index)=> {
+            countryList.push(item.country);
+            // console.log(item.country)
+          })
+         
+          // You would access the array of countries using res.data.data.countries. 
+          // The first data accesses the top-level data property of the response, 
+          // and the second data accesses the countries property within that object.
+        }
+      })
+    }catch(error) {
+      console.log(error.message);   
+    }
+  }
+
+useEffect (()=> {
+  getCountry();
+},[]);
+
   return (
     <div className="header">
-      <div className="container">
+      <div className="container-fluid">
         <div className="row align-items-center">
           <div className="col-lg-2">
             <h1>Logo</h1>
           </div>
           <div className="col-lg-5">
             <div className="headersearch d-flex align-items-center">
-                <div className="header-dropdown cursor">
-                    12345
-                   <Select/>
-                </div>
+            <Select data={Categories} placeholder={'All Categories'} icon={false}/>
                 <div className="header-search">
                     <input type="text" placeholder="Search for items..."/>
                     <SearchIcon className="search-icon"/>
                 </div>
             </div>
+          </div>
+          <div className="col-lg-5 ">
+            <div className="d-flex align-items-center">
+            <div className="country d-flex align-items-center">
+          
+               <Select data={countryList} placeholder={'Your Location'} className="cus-country" icon={<LocationOnOutlinedIcon className="select-location"/>}/>
+       
+            </div>
+            <ul className="compare-section">
+              <li className="cursor"><span><img src={compare} alt="compare"/></span>Compare</li>
+              <li className="cursor"><span><ShoppingCartOutlinedIcon/></span>Cart</li>
+              <li className="cursor position-relative"><span><PersonOutlineOutlinedIcon/></span>Account
+                <ul className="dropdown-menu">
+                    <li><Button>My Account</Button></li>
+                    <li><Button>Order Tracking</Button></li>
+                    <li><Button>Whish List</Button></li>
+                    <li><Button>Logout</Button></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
           </div>
         </div>
       </div>
--- src/components/Selectdrop/Select.js
+++ src/components/Selectdrop/Select.js
@@ -1,11 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import './Select.css';
+import { ClickAwayListener } from '@mui/base/ClickAwayListener';
 
-const Select = () => {
+
+const Select = ({data, placeholder, icon}) => {
+    // useState import hook
+    // usestate returns an array with two elements
+    // OpenSelect is the current state value
+    // setOpenSelect is used the update the state
+    const [isOpenSelect , setisOpenSelect] = useState(false);
+    const [selectedIndex, setselectedIndex]= useState(0);
+    const [selectedItem , setSelectedItem] = useState(placeholder);
+    const [listData , setlistData] = useState(data);
+    const [listData1 , setlistData1] = useState(data);
+    const OpenSelect = () => {
+        setisOpenSelect(!isOpenSelect)
+    }
+    const closeSelect =(index, name)=> {
+        setselectedIndex(index);
+        setisOpenSelect(false);
+        setSelectedItem(name);
+    }
+    const filterList = (e) => {
+        const keyword = e.target.value.toLowerCase();
+        const list = listData1.filter((item)=>{
+            return item.toLowerCase().includes(keyword);
+        })
+        const list1 = list.filter((item, index) => list.indexOf(item) === index);
+        setlistData(list1)
+        console.log(keyword)
+    }
+
     return(
-        <div className="select-dropdown">
-1
-        </div>
+        <ClickAwayListener onClickAway={()=>setisOpenSelect(false)}>
+        <div className="header-dropdown cursor">
+            {icon}
+            <span className="open-select cursor" onClick={OpenSelect}>{selectedItem.length>14? selectedItem.substr(0 , 14) + '...' : selectedItem} <KeyboardArrowDownIcon className="arrow"/></span>
+            {
+                isOpenSelect === true &&
+                <div className="select-dropdown">
+                <input type="text" placeholder="Search here ..." onChange={filterList}/>
+                <ul>
+                    {/* This is the first list item element being rendered. */}
+                    {/* It has a key attribute set to 0, which is used by React to identify list items uniquely when rendering lists dynamically. */}
+                <li key={0} onClick={()=>closeSelect(0, placeholder)} className={`${selectedIndex===0 ? 'active' : ''}`}>{placeholder}</li>
+                {
+                    // . Here, the Categories data is passed to the Select component as a prop named data. This prop is then accessed within the Select component using props.data.
+               listData.map((item, index) =>{
+                    return(
+                        // The closeSelect function is called with arguments index+1 (to avoid a collision with the first list item, which has an index of 0) and the item from the data array.
+                        <li key={index} onClick={()=>closeSelect(index+1, item)} className={`${selectedIndex===index+1 ? 'active' : ''}`}>{item}</li>
+                    )
+                })
+             }
+                    {/* select filed befor mapping */}
+                    {/* <li onClick={()=>closeSelect(0, 'All Categories')} className={`${selectedIndex===0 ? 'active' : ''}`}>All Categories</li>
+                    <li onClick={()=>closeSelect(1, 'Milks and Dhals')} className={`${selectedIndex===1 ? 'active' : ''}`}>Milks and Dhals</li>
+                    <li onClick={()=>closeSelect(2, 'Milks and Dhals')} className={`${selectedIndex===2 ? 'active' : ''}`}>Milks and Dhals</li>
+                    <li onClick={()=>closeSelect(3, 'Milks and Dhals')} className={`${selectedIndex===3 ? 'active' : ''}`}>Milks and Dhals</li>
+                    <li onClick={()=>closeSelect(4, 'Milks and Dhals Rice')} className={`${selectedIndex===4 ? 'active' : ''}`}>Milks and Dhals Rice</li>
+                    <li onClick={()=>closeSelect(5, 'Milks and Dhals')} className={`${selectedIndex===5 ? 'active' : ''}`}>Milks and Dhals</li> */}
+                </ul>
+            </div>
+            }
+       
+    </div>
+    </ClickAwayListener>
     )
 }
 
